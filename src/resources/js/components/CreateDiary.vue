b97dc8a0f4f4c9c5d231c0bd85951a3e11bc34ff
野村勇輔 <NomuraYusuke@nomuraisamutasukunoMacBook-Pro.local>
2022-01-02 16:26:48 +0900

    [mod]新規作成ページの各フォーム作成

--- src/resources/js/components/CreateDiary.vue
+++ src/resources/js/components/CreateDiary.vue
@@ -4,14 +4,71 @@
             <form>
                 <div class="form-group">
                     <label for="title">タイトル</label>
-                    <input type="text" class="form-control" id="title" aria-describedby="emailHelp" placeholder="この日の釣りの題名を入力">
+                    <input type="text" name="dairy[title]" class="form-control" id="title" placeholder="この日の釣りの題名を入力">
                 </div>
-                <button type="button" class="btn btn-primary">ボタン</button>
+                <div>
+                    <label>釣り日時</label>
+                    <input type="datetime-local" name="dairy[start_time]" class="form-control" id="start_time" placeholder="釣り開始日時">
+                    <input type="datetime-local" name="dairy[end_time]" class="form-control" id="end_time" placeholder="釣り終了日時">
+                </div>
+                <div>
+                    <label for="field">釣り場</label>
+                    <select name="field" class="form-control" id="field">
+                        <option value="伊庭内湖">伊庭内湖</option>
+                        <option value="油ヶ淵">油ヶ淵</option>
+                        <option value="その他">その他</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="season">季節</label>
+                    <select name="season" class="form-control" id="season">
+                        <option value="春">春</option>
+                        <option value="夏">夏</option>
+                        <option value="秋">秋</option>
+                        <option value="冬">冬</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="weather">天気</label>
+                    <select name="weather" class="form-control" id="weather">
+                        <option value="晴れ">晴れ</option>
+                        <option value="曇り">曇り</option>
+                        <option value="雨">雨</option>
+                        <option value="荒天">荒天</option>
+                    </select>
+                </div>
+                <div>
+                    <label>気温</label>
+                    <input type="number" name="dairy[lowest_temperature]" class="form-control" id="lowest_temperature" placeholder="最低気温(℃)">
+                    <input type="number" name="dairy[highest_temperature]" class="form-control" id="highest_temperature" placeholder="最高気温(℃)">
+                </div>
+                <div>
+                    <label>水位</label>
+                    <input type="number" name="dairy[lowest_water_level]" class="form-control" id="lowest_water_level" placeholder="最低水位(cm)">
+                    <input type="number" name="dairy[highest_water_level]" class="form-control" id="highest_water_level" placeholder="最高気温(cm)">
+                </div>
+                <div>
+                    <label for="tide">潮</label>
+                    <select name="tide" class="form-control" id="tide">
+                        <option value="小潮">小潮</option>
+                        <option value="中潮">中潮</option>
+                        <option value="大潮">大潮</option>
+                    </select>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="competition_flg" checked>
+                    <label class="form-check-label" for="competition_flg">試合フラグ</label>
+                </div>
+                <div class="form-group">
+                    <label for="consideration">詳細</label>
+                    <textarea name="consideration" class="form-control" id="consideration" rows="10"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">ボタン</button>
             </form>
         </div>
     </div>
 </template>
 
 <script>
-    console.log('aaa');
+
 </script>
