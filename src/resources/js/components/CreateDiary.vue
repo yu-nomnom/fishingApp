b5ae4813eb9dd0242910f3671346addaedc7eb3d
Yusuke Nomura <NomuraYusuke@nomuraisamutasukunoMacBook-Pro.local>
2022-02-17 00:20:42 +0900

    全てのformの値をリクエストで渡るようにした

--- src/resources/js/components/CreateDiary.vue
+++ src/resources/js/components/CreateDiary.vue
@@ -4,54 +4,54 @@
             <form>
                 <div class="form-group">
                     <label for="title">タイトル</label>
-                    <input v-model="title" type="text" name="title" class="form-control" id="title" placeholder="この日の釣りの題名を入力">
+                    <input v-model="dairyData.title" type="text" name="title" class="form-control" id="title" placeholder="この日の釣りの題名を入力">
                 </div>
                 <div>
                     <label>釣り日時</label>
-                    <input type="datetime-local" name="start_time" class="form-control" id="start_time" placeholder="釣り開始日時">
-                    <input type="datetime-local" name="end_time" class="form-control" id="end_time" placeholder="釣り終了日時">
+                    <input v-model="dairyData.start_time" type="datetime-local" name="start_time" class="form-control" id="start_time" placeholder="釣り開始日時">
+                    <input v-model="dairyData.end_time" type="datetime-local" name="end_time" class="form-control" id="end_time" placeholder="釣り終了日時">
                 </div>
                 <div>
                     <label for="field">釣り場</label>
-                    <select name="field" v-model="selected" class="form-control">
+                    <select name="field" v-model="dairyData.field" class="form-control">
                         <option v-for="field in fieldList" :key="field" :value="field">{{field}}</option>
                     </select>
                 </div>
                 <div>
                     <label for="season">季節</label>
-                    <select name="season" v-model="selected" class="form-control">
+                    <select name="season" v-model="dairyData.season" class="form-control">
                         <option v-for="season in seasonList" :key="season" :value="season">{{season}}</option>
                     </select>
                 </div>
                 <div>
                     <label for="weather">天気</label>
-                    <select name="weather" v-model="selected" class="form-control">
+                    <select name="weather" v-model="dairyData.weather" class="form-control">
                         <option v-for="weather in weatherList" :key="weather" :value="weather">{{weather}}</option>
                     </select>
                 </div>
                 <div>
                     <label>気温</label>
-                    <input type="number" name="dairy[lowest_temperature]" class="form-control" id="lowest_temperature" placeholder="最低気温(℃)">
-                    <input type="number" name="dairy[highest_temperature]" class="form-control" id="highest_temperature" placeholder="最高気温(℃)">
+                    <input v-model="dairyData.lowest_temperature" type="number" name="lowest_temperature" class="form-control" id="lowest_temperature" placeholder="最低気温(℃)">
+                    <input v-model="dairyData.highest_temperature" type="number" name="highest_temperature" class="form-control" id="highest_temperature" placeholder="最高気温(℃)">
                 </div>
                 <div>
                     <label>水位</label>
-                    <input type="number" name="dairy[lowest_water_level]" class="form-control" id="lowest_water_level" placeholder="最低水位(cm)">
-                    <input type="number" name="dairy[highest_water_level]" class="form-control" id="highest_water_level" placeholder="最高気温(cm)">
+                    <input v-model="dairyData.lowest_water_level" type="number" name="lowest_water_level" class="form-control" id="lowest_water_level" placeholder="最低水位(cm)">
+                    <input v-model="dairyData.highest_water_level" type="number" name="highest_water_level" class="form-control" id="highest_water_level" placeholder="最高水位(cm)">
                 </div>
                 <div>
                     <label for="tide">潮</label>
-                    <select name="tide" v-model="selected" class="form-control">
+                    <select name="tide" v-model="dairyData.tide" class="form-control">
                         <option v-for="tide in tideList" :key="tide" :value="tide">{{tide}}</option>
                     </select>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="competition_flg" checked>
-                    <label class="form-check-label" for="competition_flg">試合フラグ</label>
+                    <input v-model="dairyData.competition_flg" class="form-check-input" type="checkbox" id="competition_flg">
+                    <label class="form-check-label">試合フラグ</label>
                 </div>
                 <div class="form-group">
-                    <label for="consideration">詳細</label>
-                    <textarea name="consideration" class="form-control" id="consideration" rows="10"></textarea>
+                    <label>詳細</label>
+                    <textarea v-model="dairyData.consideration" class="form-control" id="consideration" rows="10"></textarea>
                 </div>
                 <button type="button" class="btn btn-primary" @click="regist()">ボタン</button>
             </form>
@@ -65,7 +65,21 @@
 export default {
     data()  {
         return {
-            title: '',
+            dairyData: {
+                title: '',
+                start_time: '',
+                end_time: '',
+                field: '',
+                season: '',
+                weather: '',
+                lowest_temperature: '',
+                highest_temperature: '',
+                lowest_water_level: '',
+                highest_water_level: '',
+                tide: '',
+                competition_flg: false,
+                consideration: '',
+            },
             weatherList : [],
             seasonList : [],
             tideList : [],
@@ -84,9 +98,13 @@ export default {
         }
     },
     methods: {
+        checkCompetitionFlg() {
+            console.log('checkCompetitionFlg');
+            this.dairyData.competition_flg = true;
+        },
         regist() {
             axios.post('/api/diary_regist', {
-                title: this.title
+                dairyData: this.dairyData 
             })
             .then((res) => {
                 console.log('bbbb');
